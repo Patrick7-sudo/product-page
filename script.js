6cf54711bf58327a3cb972b9a117ed2811671984
patrick <tasniim_bari@yahoo.com>
2021-06-24 18:42:44 +0100

    in-progress for navbar and header-mcompleted some javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+const barBtn = document.querySelector('.icon-button');
+const options = document.querySelector('.logo-options');
+
+// window.addEventListener()
+
+barBtn.addEventListener('click',function(){
+    if(options.classList.contains('hidden')){
+        options.classList.remove('hidden')
+    }else{options.classList.add('hidden')}
+})
+console.log(`hello`)
